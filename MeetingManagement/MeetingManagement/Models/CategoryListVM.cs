4ab5154dbd3248829cc103cedc122d01800f0684
Khon Vu <vu.187pm14039@vanlanguni.vn>
2021-07-08 20:27:54 +0700

    [MM-149] - Update progress bar

---
+++ MeetingManagement/MeetingManagement/Models/CategoryListVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MeetingManagement.Models
+{
+    public class CategoryListVM
+    {
+        public int CateId { get; set; }
+        public String CateName { get; set; }
+        public String CateContent { get; set; }
+        public String CateCreateBy { get; set; }
+    }
+}
