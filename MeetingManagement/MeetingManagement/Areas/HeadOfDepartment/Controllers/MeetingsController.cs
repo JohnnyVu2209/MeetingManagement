039b26c967b40d30a3b85ab168d7560c66b80496
yukihatake1804 <nhokhieuthao@gmail.com>
2021-07-13 22:51:28 +0700

    Model

--- MeetingManagement/MeetingManagement/Areas/HeadOfDepartment/Controllers/MeetingsController.cs
+++ MeetingManagement/MeetingManagement/Areas/HeadOfDepartment/Controllers/MeetingsController.cs
@@ -165,6 +165,25 @@ namespace MeetingManagement.Areas.HeadOfDepartment.Controllers
             var meeting = db.MEETINGs.Find(id);
             return View(meeting);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult MeetingDetail(int category_id, int meeting_id, string meeting_name, DateTime date_start, TimeSpan time_start, string location, string content, int status)
+        {
+            MEETING meeting = db.MEETINGs.Find(meeting_id);
+            meeting.Category_id = category_id;
+            meeting.Meeting_name = meeting_name;
+            meeting.Date_Start = date_start;
+            meeting.Time_Start = time_start;
+            meeting.Location = location;
+            meeting.Meeting_content = content;
+            meeting.Status = status;
+
+            db.Entry(meeting).State = EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
         public PartialViewResult MeetingInfo(int id)
         {
             var meeting = db.MEETINGs.Find(id);
