50f96b4cd0b5277317183e45d5e8db7f23f58f5a
NguyenDaiLam <lam.187pm13948@vanlanguni.vn>
2021-07-18 20:17:47 +0700

    Commit

--- MeetingManagement/MeetingManagement/Models/Models.Context.cs
+++ MeetingManagement/MeetingManagement/Models/Models.Context.cs
@@ -36,6 +36,7 @@ namespace MeetingManagement.Models
         public virtual DbSet<MEETING_STATUS> MEETING_STATUS { get; set; }
         public virtual DbSet<MEMBER> MEMBERs { get; set; }
         public virtual DbSet<OTHER_ACCOUNT> OTHER_ACCOUNTs { get; set; }
+        public virtual DbSet<REPORT> REPORTs { get; set; }
         public virtual DbSet<sysdiagram> sysdiagrams { get; set; }
         public virtual DbSet<TASK> TASKs { get; set; }
     }
