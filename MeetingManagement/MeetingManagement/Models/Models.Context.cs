6180b7c00a66a7cafb84bba9a56034b3f4bed790
187PM06719 - Doan Nguyen Thong - K24T-PM1 <thong.187pm06719@vanlanguni.vn>
2021-07-19 15:14:17 +0700

    FIx

--- MeetingManagement/MeetingManagement/Models/Models.Context.cs
+++ MeetingManagement/MeetingManagement/Models/Models.Context.cs
@@ -26,6 +26,7 @@ namespace MeetingManagement.Models
         }
     
         public virtual DbSet<C__MigrationHistory> C__MigrationHistory { get; set; }
+        public virtual DbSet<sysdiagram> sysdiagrams { get; set; }
         public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
         public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
         public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
@@ -37,7 +38,6 @@ namespace MeetingManagement.Models
         public virtual DbSet<MEMBER> MEMBERs { get; set; }
         public virtual DbSet<OTHER_ACCOUNT> OTHER_ACCOUNTs { get; set; }
         public virtual DbSet<REPORT> REPORTs { get; set; }
-        public virtual DbSet<sysdiagram> sysdiagrams { get; set; }
         public virtual DbSet<TASK> TASKs { get; set; }
     }
 }
