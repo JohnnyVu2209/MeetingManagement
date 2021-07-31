5ec261c2f05914cef0f445e7186e52328d7d91a5
yukihatake1804 <nhokhieuthao@gmail.com>
2021-07-31 23:49:52 +0700

    Fix UI

--- MeetingManagement/MeetingManagement/Areas/HeadOfDepartment/Controllers/MainPageController.cs
+++ MeetingManagement/MeetingManagement/Areas/HeadOfDepartment/Controllers/MainPageController.cs
@@ -36,16 +36,10 @@ namespace MeetingManagement.Areas.HeadOfDepartment.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult VerifyMeeting(int category_id, int meeting_id, string meeting_name, DateTime date_start, TimeSpan time_start, string location, string content, int status)
+        public ActionResult VerifyMeeting(int? meeting_id)
         {
-            MEETING meeting = db.MEETINGs.Find(meeting_id);
-            meeting.Category_id = category_id;
-            meeting.Meeting_name = meeting_name;
-            meeting.Date_Start = date_start;
-            meeting.Time_Start = time_start;
-            meeting.Location = location;
-            meeting.Meeting_content = content;
-            meeting.Status = status;
+            var meeting = db.MEETINGs.Find(meeting_id);
+            meeting.Status = 2;
 
             db.Entry(meeting).State = EntityState.Modified;
             db.SaveChanges();
@@ -92,10 +86,6 @@ namespace MeetingManagement.Areas.HeadOfDepartment.Controllers
 
 
         public ActionResult StaticticBCN() {
-
-
-
-
             return View();
 
         }
