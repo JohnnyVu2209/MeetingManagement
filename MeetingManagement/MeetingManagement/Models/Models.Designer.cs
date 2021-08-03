2ff6cadeaf27726ff77cca106e9f5847b35b99ca
NguyenDaiLam <lam.187pm13948@vanlanguni.vn>
2021-08-03 12:03:56 +0700

    Merge Code

--- MeetingManagement/MeetingManagement/Models/Models.Designer.cs
+++ MeetingManagement/MeetingManagement/Models/Models.Designer.cs
@@ -1,7 +1,4 @@
-
-// T4 code generation is enabled for model 'D:\SUBJECT\SEP\Source Code\MM2\MeetingManagement\MeetingManagement\Models\Models.edmx'. 
-
-
+﻿// T4 code generation is enabled for model 'C:\Users\Admin\Source\Repos\MeetingManagement\MeetingManagement\MeetingManagement\Models\Models.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
