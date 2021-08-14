70880f2debf91c4cf07c5231d69431b260c298dd
nkhoi2000 <45933579+nkhoi2000@users.noreply.github.com>
2021-08-14 14:29:37 +0700

    Complete Meeting

--- MeetingManagement/MeetingManagement/Areas/HeadOfDepartment/Controllers/MEETINGsController.cs
+++ MeetingManagement/MeetingManagement/Areas/HeadOfDepartment/Controllers/MEETINGsController.cs
@@ -145,16 +145,16 @@ namespace MeetingManagement.Areas.HeadOfDepartment.Controllers
         [HttpPost]
         public ActionResult CancelModel(int Meeting_id, MEETING model)
         {
-                var meeting = db.MEETINGs.Find(Meeting_id);
-                //change meeting status
-                meeting.Status = 7;
-                meeting.Feedback = model.Feedback;
+            var meeting = db.MEETINGs.Find(Meeting_id);
+            //change meeting status
+            meeting.Status = 7;
+            meeting.Feedback = model.Feedback;
 
-                db.Entry(meeting).State = EntityState.Modified;
-                db.SaveChanges();
-                if(meeting.Verify_by!=null)
-                    sendCancelModel(meeting);
-                return RedirectToAction("Index","tabCuocHop");
+            db.Entry(meeting).State = EntityState.Modified;
+            db.SaveChanges();
+            if (meeting.Verify_by != null)
+                sendCancelModel(meeting);
+            return RedirectToAction("Index", "tabCuocHop");
         }
 
         private void sendCancelModel(MEETING meeting)
@@ -602,7 +602,7 @@ namespace MeetingManagement.Areas.HeadOfDepartment.Controllers
 
         private bool ValidateFeedback(MEETING meeting)
         {
-            if(meeting.Feedback == null || meeting.Feedback == "")
+            if (meeting.Feedback == null || meeting.Feedback == "")
             {
                 return true;
             }
@@ -771,7 +771,7 @@ namespace MeetingManagement.Areas.HeadOfDepartment.Controllers
          *************************REPORT************************
          */
 
-        
+
 
         public ActionResult NotiMeeting(int meetingId)
         {
@@ -798,6 +798,14 @@ namespace MeetingManagement.Areas.HeadOfDepartment.Controllers
                 mail.SendMail();
             }
             return RedirectToAction("ReportList", "Meetings", new { id = categoryId });
+        }
+        public ActionResult MeetingComplete(int id)
+        {
+            var meeting = db.MEETINGs.Find(id);
+            meeting.Status = 5;
+            db.Entry(meeting).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("MeetingDetail", "Meetings", new { id = meeting.Meeting_id, modify = false });
         }
     }
 }
